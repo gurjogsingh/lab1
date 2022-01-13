4834a3ca7eb1140852a612ffaf6bfc14e04a6e9b
gurjogsingh <gurjog@ualberta.ca>
2022-01-12 19:37:24 -0500

    fin

--- lab1.py
+++ lab1.py
@@ -5,6 +5,7 @@ print(requests.__version__)
 url = "http://google.com/"
 print(requests.get(url).status_code)
 
-urlRaw = "https://raw.github.com/django/django/master/setup.py"
+
+urlRaw = "https://raw.githubusercontent.com/gurjogsingh/lab1/master/lab1.py"
 toPrint = requests.get(urlRaw)
 print(toPrint._content)
